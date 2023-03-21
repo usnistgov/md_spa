9ec03cc31295a8cee19e3352fc6abf3647b2a6e3
jac16 <jennifer.clark@nist.gov>
2023-03-21 16:38:39 -0400

    Bug fix check_wrap

--- md_spa/misc_functions.py
+++ md_spa/misc_functions.py
@@ -90,7 +90,7 @@ def check_wrap(pos_diff, dimensions, dist_type="distance"):
 t, wrapped if approproate.
 
     """
-    dimensions = np.expand_dims(dimensions, (0,1))
+    dimensions = np.expand_dims(dimensions, tuple(range(len(np.shape(pos_diff))-1)))
     if dist_type == "distance":
         images = ( dimensions/2 - pos_diff ) // dimensions
     elif dist_type == "wrap":
